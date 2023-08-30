b84ce6a99553a57dfab325a00cc1a4bd2f6e52d9
Lekan <jimoridwanlekan@gmail.com>
2023-08-30 16:40:39 +0100

    Add initial setup page

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import { redirect } from "next/navigation";
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+
+const Home = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
   return (
     <main className="flex flex-col min-h-screen items-center justify-between p-24">
       Hello 🤡{" "}
     </main>
   );
-}
+};
+
+export default Home;
